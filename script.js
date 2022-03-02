ac2d6381157f78c9bddb8ad8c9877aad931c1efc
Sabir59 <69711644+Sabir59@users.noreply.github.com>
2022-03-02 20:03:00 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-class CSelector{constructor(a,b){this.core=a,this.style=b}coreHandler(){const a=document.getElementById(this.core[0]),b=document.createElement("div"),c=document.createElement("p"),d=document.createElement("input");b.classList.add(this.style[0]),c.textContent=this.core[1],d.type="hidden",d.name=this.core[3],a.insertAdjacentElement("afterbegin",c),a.insertAdjacentElement("beforeend",d),a.insertAdjacentElement("beforeend",b),this.core[4].forEach(a=>{let c=document.createElement("p");c.innerText=a,c.classList.add(this.style[1]),b.insertAdjacentElement("beforeend",c)}),a.addEventListener("click",function(a){b.classList.toggle("toggle-options"),c.innerText=a.target.innerText,d.value=a.target.innerText})}init(){return this.coreHandler()}}const selector=new CSelector(["c-selector","Select","c-selector__value","c-selector",["Download","Install","Upload"]],["option-group","option"]);selector.init();
+class CSelector {
+  constructor(core, style) {
+    this.core = core;
+    this.style = style;
+  }
+
+  coreHandler() {
+    const selector = document.getElementById(this.core[0]);
+    const optionGroup = document.createElement("div");
+    const label = document.createElement("p");
+    const input = document.createElement("input");
+
+    optionGroup.classList.add(this.style[1]);
+    label.textContent = this.core[1];
+    label.className = this.style[0];
+
+    input.type = "hidden";
+    input.name = this.core[3];
+    input.id = this.core[2];
+
+    selector.insertAdjacentElement("afterbegin", label);
+    selector.insertAdjacentElement("beforeend", input);
+
+    selector.insertAdjacentElement("beforeend", optionGroup);
+
+    this.core[4].forEach((value) => {
+      let option = document.createElement("p");
+      option.innerText = value;
+      option.classList.add(this.style[2]);
+
+      optionGroup.insertAdjacentElement("beforeend", option);
+    });
+
+    selector.addEventListener("click", (event) => {
+      optionGroup.classList.toggle(this.style[3]);
+      label.innerText = event.target.innerText;
+      input.value = event.target.innerText;
+    });
+  }
+
+  init() {
+    return this.coreHandler();
+  }
+}
+
+const selector = new CSelector(
+  [
+    "c-selector",
+    "Select",
+    "c-selector__input",
+    "c-selector",
+    ["Download", "Install", "Upload"],
+  ],
+  ["label", "option-group", "option", "show-options"]
+);
+
+selector.init();
